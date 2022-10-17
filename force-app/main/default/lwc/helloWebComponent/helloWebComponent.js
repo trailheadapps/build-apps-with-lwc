b1443529ccacdf7f96dc98a41e5869435e02a91c
Diogo Marques <djcm.marques.95@gmail.com>
2022-10-17 17:15:00 +0100

    1

--- force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
+++ force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
+export default class HelloWebComponent extends LightningElement {
+	greeting = 'Trailblazer';
 
-export default class HelloWebComponent extends LightningElement {}
+    handleGreetingChange(event) {
+        this.greeting = event.target.value;
+    }
+    currentDate = new Date().toDateString();
+        get capitalizedGreeting() {
+	return `Hello ${this.greeting.toUpperCase()}!`;
+}
+}
